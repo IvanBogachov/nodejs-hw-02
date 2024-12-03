4c6bf0140a4136b26905897ca5f6673730b1a049
Ivan_Bog <24term@gmail.com>
2024-12-03 11:12:07 +0200

    add ....

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
-import { getEnvVar } from './utils/getEnvVar.js';
+import { env } from './utils/getEnvVar.js';
+import { getAllStudents, getStudentById } from './services/students.js';
 
-const PORT = Number(getEnvVar('PORT', '3000'));
+
+const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
@@ -41,5 +43,30 @@ export const startServer = () => {
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+  });
+  app.get('/students', async (req, res) => {
+    const students = await getAllStudents();
+
+    res.status(200).json({
+      data: students,
+    });
+  });
+
+  app.get('/students/:studentId', async (req, res, next) => {
+    const { studentId } = req.params;
+    const student = await getStudentById(studentId);
+
+    // Відповідь, якщо контакт не знайдено
+    if (!student) {
+      res.status(404).json({
+        message: 'Student not found',
+      });
+      return;
+    }
+
+    // Відповідь, якщо контакт знайдено
+    res.status(200).json({
+      data: student,
+    });
   });
 };
