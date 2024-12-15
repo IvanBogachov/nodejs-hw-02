52d1e1684a7c96fcfb3d4ae28d2653ee448ba127
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-12-15 16:29:01 +0200

    add ....

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -7,7 +7,6 @@ export const validateBody = (schema) => async (req, res, next) => {
     });
     next();
   } catch (err) {
-    const errorDetails = err.details?.map((detail) => detail.message) || [];
     const error = createHttpError(400, 'Bad Request', {
       errors: err.details,
     });
